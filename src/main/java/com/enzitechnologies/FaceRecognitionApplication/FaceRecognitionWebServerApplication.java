4261f9154172fe22654d4684b1c3a3c24ec2578a
KahigaKiguru <dev.enzitechnologies@gmail.com>
2020-11-19 18:11:47 +0300

    first commit

---
+++ src/main/java/com/enzitechnologies/FaceRecognitionApplication/FaceRecognitionWebServerApplication.java
@@ -0,0 +1,13 @@
+package com.enzitechnologies.FaceRecognitionApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceRecognitionWebServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceRecognitionWebServerApplication.class, args);
+	}
+
+}
